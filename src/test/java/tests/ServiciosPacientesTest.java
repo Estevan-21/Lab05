0ce40ac54876b6516aedaa7eb6cf1126da9a797b
vdavid30 <spm1dh@gmail.com>
2017-09-21 11:48:32 -0500

    Pom arreglado

--- src/test/java/tests/ServiciosPacientesTest.java
+++ src/test/java/tests/ServiciosPacientesTest.java
@@ -5,6 +5,11 @@
  */
 package tests;
 
+import edu.eci.pdsw.samples.entities.Eps;
+import edu.eci.pdsw.samples.entities.Paciente;
+import edu.eci.pdsw.samples.services.ExcepcionServiciosPacientes;
+import edu.eci.pdsw.samples.services.ServiciosHistorialPacientesFactory;
+import edu.eci.pdsw.samples.services.ServiciosPacientes;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -14,12 +19,20 @@ import static org.junit.Assert.*;
  * @author hcadavid
  */
 public class ServiciosPacientesTest {
-    
+    private ServiciosPacientes servicepacientes;
     public ServiciosPacientesTest() {
     }
     
     @Before
     public void setUp() {
+            servicepacientes = ServiciosHistorialPacientesFactory.getInstance().getServiciosPaciente();
+
     }
-    
+    @Test
+    public void registraElPaciente() throws ExcepcionServiciosPacientes{
+        Eps eps1 = new Eps("Compensar", "7289374982-0");
+        Paciente paciente1 = new Paciente(11111,"CC", "Juan Perez", java.sql.Date.valueOf("2000-01-01"), eps1);
+        servicepacientes.registrarNuevoPaciente(paciente1);
+        //assertTrue(paciente1.equals(servicepacientes.consultarPaciente(1, "11111")));
+    }
 }
